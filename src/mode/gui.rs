d9e314d7dc22994e1b42e714ca90d283f3d86890
XenorInspire <nicolas.fouchard94370@gmail.com>
2023-08-10 11:35:56 +0200

    ADD gui functions from password module to gui

--- src/mode/gui.rs
+++ src/mode/gui.rs
@@ -1,5 +1,5 @@
 // Internal crates
-use crate::password;
+use crate::password::{self, PasswordConfig};
 use crate::system;
 
 pub fn run(){
@@ -9,7 +9,7 @@ pub fn run(){
         match &*choice {
             "0" => break,
             // "1" => generate_wordlist(),
-            "2" => password::main_passwd_generation(),
+            "2" => main_passwd_generation(),
             // "3" => benchmark_cpu(),
             _ => (),
         }
@@ -48,4 +48,105 @@ fn print_menu() {
     println!("2 : Generate random password(s)");
     println!("3 : Benchmark CPU");
     println!("0 : Exit WorgenX");
+}
+
+// This is the main function of the random password generation module
+fn main_passwd_generation() {
+    let mut again = String::from("y");
+
+    while again.eq("y") {
+        let password_config = allocate_passwd_config_gui();
+
+        println!("How many passwords do you want to generate ?");
+        let number_of_passwords = system::get_user_choice_int();
+
+        println!("Do you want to save the passwords in a file ? (y/n)");
+        let choice = system::get_user_choice_yn();
+        let passwords = password::generate_random_passwords(&password_config, number_of_passwords);
+        if choice.eq("y") {
+            system::save_passwords_into_a_file(&passwords)
+        };
+
+        println!("You can find your password(s) below :");
+        for password in passwords {
+            println!("{}", password);
+        }
+
+        println!("\nDo you want to generate another password(s) ? (y/n)");
+        again = system::get_user_choice_yn();
+    }
+}
+
+/// This function is charged to allocate the password config structure from the user's choices in the GUI
+///
+/// # Example
+/// ```
+/// let password_config = allocate_passwd_config_gui();
+/// ```
+/// # Returns
+/// 
+/// The password config structure named PasswordConfig
+/// 
+fn allocate_passwd_config_gui() -> PasswordConfig {
+    let mut password_config = PasswordConfig {
+        numbers: false,
+        special_characters: false,
+        uppercase: false,
+        lowercase: false,
+        length: 0,
+    };
+    let mut choice;
+    let mut is_option_chosen = false;
+
+    while !is_option_chosen {
+        println!("\nChoose what your password is composed of :");
+        println!("Uppercase letters (A-Z) ? (y/n)");
+        choice = system::get_user_choice_yn();
+        match &*choice {
+            "y" => {
+                password_config.uppercase = true;
+                is_option_chosen = true;
+            }
+            _ => (),
+        }
+
+        println!("Lowercase letters (a-z) ? (y/n)");
+        choice = system::get_user_choice_yn();
+        match &*choice {
+            "y" => {
+                password_config.lowercase = true;
+                is_option_chosen = true;
+            }
+            _ => (),
+        }
+
+        println!("Numbers (0-9) ? (y/n)");
+        choice = system::get_user_choice_yn();
+        match &*choice {
+            "y" => {
+                password_config.numbers = true;
+                is_option_chosen = true;
+            }
+            _ => (),
+        }
+
+        println!("Special characters ? (y/n)");
+        choice = system::get_user_choice_yn();
+        match &*choice {
+            "y" => {
+                password_config.special_characters = true;
+                is_option_chosen = true;
+            }
+            _ => (),
+        }
+
+        if !is_option_chosen {
+            println!("You must choose at least one option !");
+        }
+    }
+
+    println!("How long do you want your password to be ?");
+    password_config.length = system::get_user_choice_int();
+
+    password_config
 }
--- src/password.rs
+++ src/password.rs
@@ -1,119 +1,17 @@
- // Internal crates
+// Internal crates
 use crate::dict;
-use crate::system;
 
 // External crates
 use rand::{seq::SliceRandom, Rng};
 
 /// This struct built from the user's choices will be used to generate the random password
-/// 
-struct PasswordConfig {
+///
+pub struct PasswordConfig {
     pub numbers: bool,
     pub special_characters: bool,
     pub uppercase: bool,
     pub lowercase: bool,
     pub length: u64,
-}
-
-// This is the main function of the random password generation module
-pub fn main_passwd_generation() {
-    let mut again = String::from("y");
-
-    while again.eq("y") {
-        let password_config = allocate_passwd_config_gui();
-
-        println!("How many passwords do you want to generate ?");
-        let number_of_passwords = system::get_user_choice_int();
-
-        println!("Do you want to save the passwords in a file ? (y/n)");
-        let choice = system::get_user_choice_yn();
-        let passwords = generate_random_passwords(&password_config, number_of_passwords);
-        if choice.eq("y") {
-            system::save_passwords_into_a_file(&passwords)
-        };
-
-        println!("You can find your password(s) below :");
-        for password in passwords {
-            println!("{}", password);
-        }
-
-        println!("\nDo you want to generate another password(s) ? (y/n)");
-        again = system::get_user_choice_yn();
-    }
-}
-
-/// This function is charged to allocate the password config structure from the user's choices in the GUI
-///
-/// # Example
-/// ```
-/// let password_config = allocate_passwd_config_gui();
-/// ```
-/// # Returns
-/// 
-/// The password config structure named PasswordConfig
-/// 
-fn allocate_passwd_config_gui() -> PasswordConfig {
-    let mut password_config = PasswordConfig {
-        numbers: false,
-        special_characters: false,
-        uppercase: false,
-        lowercase: false,
-        length: 0,
-    };
-    let mut choice;
-    let mut is_option_chosen = false;
-
-    while !is_option_chosen {
-        println!("\nChoose what your password is composed of :");
-        println!("Uppercase letters (A-Z) ? (y/n)");
-        choice = system::get_user_choice_yn();
-        match &*choice {
-            "y" => {
-                password_config.uppercase = true;
-                is_option_chosen = true;
-            }
-            _ => (),
-        }
-
-        println!("Lowercase letters (a-z) ? (y/n)");
-        choice = system::get_user_choice_yn();
-        match &*choice {
-            "y" => {
-                password_config.lowercase = true;
-                is_option_chosen = true;
-            }
-            _ => (),
-        }
-
-        println!("Numbers (0-9) ? (y/n)");
-        choice = system::get_user_choice_yn();
-        match &*choice {
-            "y" => {
-                password_config.numbers = true;
-                is_option_chosen = true;
-            }
-            _ => (),
-        }
-
-        println!("Special characters ? (y/n)");
-        choice = system::get_user_choice_yn();
-        match &*choice {
-            "y" => {
-                password_config.special_characters = true;
-                is_option_chosen = true;
-            }
-            _ => (),
-        }
-
-        if !is_option_chosen {
-            println!("You must choose at least one option !");
-        }
-    }
-
-    println!("How long do you want your password to be ?");
-    password_config.length = system::get_user_choice_int();
-
-    password_config
 }
 
 /// This function is charged to create the content of the password
@@ -123,11 +21,11 @@ fn allocate_passwd_config_gui() -> PasswordConfig {
 /// ```
 /// let password_content = create_passwd_content(&password_config);
 /// ```
-/// 
+///
 /// # Returns
-/// 
+///
 /// The vector of u8 containing the characters that will be used to generate the password
-/// 
+///
 fn create_passwd_content(password_config: &PasswordConfig) -> Vec<u8> {
     let mut password_families: Vec<Vec<u8>> = Vec::new();
 
@@ -171,11 +69,11 @@ fn create_passwd_content(password_config: &PasswordConfig) -> Vec<u8> {
 /// let mut shuffled_numbers = shuffle_dict(&dict::NUMBERS.to_vec());
 /// let mut shuffled_special_characters = shuffle_dict(&dict::SPECIAL_CHARACTERS.to_vec());
 /// ```
-/// 
+///
 /// # Returns
-/// 
+///
 /// The shuffled vector of u8 passed in parameter
-/// 
+///
 fn shuffle_dict(dict: &Vec<u8>) -> Vec<u8> {
     let mut shuffled_dict = dict.to_vec();
     let mut rng = rand::thread_rng();
@@ -184,18 +82,18 @@ fn shuffle_dict(dict: &Vec<u8>) -> Vec<u8> {
 }
 
 /// This function is charged to generate an array of random passwords
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `password_config` - The password config structure
 /// * `number_of_passwords` - The number of passwords to generate
-/// 
+///
 /// # Example
 /// ```
 /// let passwords = generate_random_passwords(&password_config, number_of_passwords);
 /// ```
-/// 
-fn generate_random_passwords(
+///
+pub fn generate_random_passwords(
     password_config: &PasswordConfig,
     number_of_passwords: u64,
 ) -> Vec<String> {
