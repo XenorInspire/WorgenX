8b91dc86d0bdb02b0cc0e1948f1c142038c5f8d8
XenorInspire <nicolas.fouchard94370@gmail.com>
2023-12-12 22:48:17 +0100

    ADD final dictionary building function

--- src/wordlist.rs
+++ src/wordlist.rs
@@ -1,4 +1,5 @@
 // Internal crates
+use crate::dict;
 
 // This struct is built from the user's choices will be used to generate the wordlist
 pub struct WordlistConfig {
@@ -7,4 +8,37 @@ pub struct WordlistConfig {
     pub uppercase: bool,
     pub lowercase: bool,
     pub mask: String,
+}
+
+/// This function is charged to build the final dictionary from the user's choices
+/// It returns a vector of u8 containing the characters that will be used to generate the wordlist
+///
+/// # Arguments
+///
+/// * `wordlist_config` - The struct containing the user's choices
+///
+/// # Returns
+///
+/// The vector of u8 containing the characters that will be used to generate the wordlist
+///
+fn create_wordlist_content(wordlist_config: &WordlistConfig) -> Vec<u8> {
+    let mut final_dict: Vec<u8> = Vec::new();
+
+    if wordlist_config.uppercase {
+        final_dict.extend(dict::UPPERCASE);
+    }
+
+    if wordlist_config.lowercase {
+        final_dict.extend(dict::LOWERCASE);
+    }
+
+    if wordlist_config.numbers {
+        final_dict.extend(dict::NUMBERS);
+    }
+
+    if wordlist_config.special_characters {
+        final_dict.extend(dict::SPECIAL_CHARACTERS);
+    }
+
+    final_dict
 }
