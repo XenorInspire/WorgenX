96111932f3e39e572409cfde0f62f163968a84bc
XenorInspire <nicolas.fouchard94370@gmail.com>
2024-05-06 18:29:55 +0200

    UPDATE content randomization for password generation to have a better entropy

--- src/password.rs
+++ src/password.rs
@@ -2,7 +2,7 @@
 use crate::dict;
 
 // External crates
-use rand::{rngs::ThreadRng, seq::SliceRandom, Rng};
+use rand::{rngs::OsRng, seq::SliceRandom, Rng};
 
 /// This struct built from the user's choices will be used to generate the random password.
 ///
@@ -25,37 +25,28 @@ pub struct PasswordConfig {
 /// # Returns
 ///
 /// The vector of u8 containing the characters that will be used to generate the password.
+/// All the characters are shuffled in a random order.
 ///
 fn create_passwd_content(password_config: &PasswordConfig) -> Vec<u8> {
-    let mut password_families: Vec<Vec<u8>> = Vec::new();
+    let mut password_content: Vec<u8> = Vec::new();
 
     if password_config.uppercase {
-        password_families.push(shuffle_dict(dict::UPPERCASE));
+        password_content.extend(shuffle_dict(dict::UPPERCASE));
     }
 
     if password_config.lowercase {
-        password_families.push(shuffle_dict(dict::LOWERCASE));
+        password_content.extend(shuffle_dict(dict::LOWERCASE));
     }
 
     if password_config.numbers {
-        password_families.push(shuffle_dict(dict::NUMBERS));
+        password_content.extend(shuffle_dict(dict::NUMBERS));
     }
 
     if password_config.special_characters {
-        password_families.push(shuffle_dict(dict::SPECIAL_CHARACTERS));
+        password_content.extend(shuffle_dict(dict::SPECIAL_CHARACTERS));
     }
 
-    // Generate the random indexes of the password families in a random order
-    let mut rng: ThreadRng = rand::thread_rng();
-    let mut password_families_indexes: Vec<usize> = (0..password_families.len()).collect();
-    password_families_indexes.shuffle(&mut rng);
-
-    // Generate the final array of u8 containing the password content
-    let mut password_content: Vec<u8> = Vec::new();
-    for i in password_families_indexes {
-        password_content.extend(password_families[i].to_vec());
-    }
-    let mut rng: ThreadRng = rand::thread_rng();
+    let mut rng: OsRng = OsRng;
     password_content.shuffle(&mut rng);
     password_content
 }
@@ -72,7 +63,7 @@ fn create_passwd_content(password_config: &PasswordConfig) -> Vec<u8> {
 ///
 fn shuffle_dict(dict: &[u8]) -> Vec<u8> {
     let mut shuffled_dict: Vec<u8> = dict.to_vec();
-    let mut rng: ThreadRng = rand::thread_rng();
+    let mut rng: OsRng = OsRng;
     shuffled_dict.shuffle(&mut rng);
     shuffled_dict
 }
@@ -93,7 +84,7 @@ pub fn generate_random_passwords(password_config: &PasswordConfig) -> Vec<String
     let password_content: Vec<u8> = create_passwd_content(password_config);
 
     for _ in 0..password_config.number_of_passwords {
-        let mut rng: ThreadRng = rand::thread_rng();
+        let mut rng: OsRng = OsRng;
         let mut password: String = String::new();
         for _ in 0..password_config.length {
             let idx: usize = rng.gen_range(0..password_content.len());
