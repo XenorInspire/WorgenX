87adbdf3ac37835338bbc20a937a1a0dcee44781
XenorInspire <nicolas.fouchard94370@gmail.com>
2024-05-06 19:05:59 +0200

    CLEAN code

--- src/json.rs
+++ src/json.rs
@@ -2,9 +2,10 @@
 use crate::password::PasswordConfig;
 
 // External crates
-use serde_json::{json, Value};
+use serde_json::json;
 
 /// This function is charged to return a JSON String of the password config structure.
+/// The output can be used to interact with other programs.
 ///
 /// # Arguments
 ///
@@ -19,16 +20,14 @@ pub fn password_config_to_json(
     password_config: &PasswordConfig,
     passwords: &Vec<String>,
 ) -> String {
-    let json: Value = json!({
+    json!({
         "number_of_passwords": password_config.number_of_passwords,
         "password_length": password_config.length,
         "uppercase": password_config.uppercase,
         "lowercase": password_config.lowercase,
         "numbers": password_config.numbers,
         "special_characters": password_config.special_characters,
-        // Display the passwords in a JSON array
         "passwords": passwords
-    });
-
-    json.to_string()
+    })
+    .to_string()
 }
