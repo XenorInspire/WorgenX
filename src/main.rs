38fa81c8a007ca77667365686fc36794d06d7823
XenorInspire <nicolas.fouchard94370@gmail.com>
2025-12-30 16:35:06 +0100

    UPDATE some comments #2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-// Prevents the use of unsafe code
 #![forbid(unsafe_code)]
 
 // Prevents the compilation of both modes which may cause conflicts
 #[cfg(all(not(feature = "gui"), not(feature = "cli")))]
 compile_error!("You must specify a mode: 'gui' or 'cli'.");
 
-// Decomment these lines if in production to prevent compilation with both modes
+// Decomment these lines if in production to prevent both modes to be compiled
 // #[cfg(all(feature = "gui", feature = "cli"))]
 // compile_error!("You must specify only one mode: 'gui' or 'cli'.");
 
@@ -21,8 +20,6 @@ mod wordlist;
 #[cfg(feature = "cli")]
 mod json;
 
-/// This function is the "entry point" of the program.
-///
 fn main() {
     #[cfg(feature = "gui")]
     mode::gui::run();
